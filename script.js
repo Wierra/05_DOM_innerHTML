b8e6bc963a28f181abd262ceaabbb738876efc3b
Věra Juračáková <juracakova.vera@gmail.com>
2024-10-21 18:47:02 +0200

    Uložení querySelector

--- script.js
+++ script.js
@@ -8,7 +8,14 @@ nadpis.style.backgroundColor = "red"
 console.log(nadpis)
 
 nadpis.classList.add("ramecek")
+nadpis.classList.remove("ramecek")  //smazání třídy
+
+
 
 document.querySelector("#img-kocka").src = "https://1gr.cz/fotky/idnes/16/102/r7/MCE66b5b9_profimedia_0131330855.jpg"
-document.querySelector("#img-kocka").style.width = "200px"
+document.querySelector("#img-kocka").style.width = "200px"  //v rámci JS pracujeme s id
 document.querySelector("#img-kocka").alt = "Malé kotě, mňau"
+
+document.querySelector(".produkty p")       //výběr prvku z určité části = kontextový selektor; univerzálnější
+
+//document.getElementById       = historický zápis, můžeme se setkat, dnes se již nepoužívá
